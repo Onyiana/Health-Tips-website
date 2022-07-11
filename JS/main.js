a8456e56f8b6645b98f5aa4f721274406e8e409b
Onyiana <adaurecananaba@gmail.com>
2022-07-11 19:06:56 +0100

    updated

--- JS/main.js
+++ JS/main.js
@@ -4,15 +4,38 @@ accordionBtn.forEach(element => {
     element.addEventListener('click', function(){
         const id = element.getAttribute('id')
         const ansClass = `answered${id}`
-        console.log(ansClass)
         const answered = document.getElementById(ansClass)
-        if (answered.style.display == 'none') {
+        if (answered.style.display === 'none') {
             element.innerHTML = '<i class="accordionBtn" id="1"><button style="cursor: pointer;">Open</button></i>'
             answered.style.display = 'block'
-        }
-        else {
+        }else {
             element.innerHTML = '<i class="accordionBtn" id="1"><button style="cursor: pointer;">Close</button></i>'
             answered.style.display = 'none'
         }
     })
 });
+
+
+const menuOpen = document.querySelector('.ri-menu-add-line')
+const menuClose = document.querySelector('.ri-close-circle-fill')
+const navMenu = document.querySelector('#navlink')
+menuOpen.addEventListener('click', function(){
+    navMenu.style.top = '50%'
+    menuOpen.style.display = 'none'
+    menuClose.style.display = 'block'
+})
+
+menuClose.addEventListener('click', function(){
+    navMenu.style.top = '-50%'
+    menuOpen.style.display = 'block'
+    menuClose.style.display = 'none'
+})
+
+const navLists = document.querySelectorAll('li')
+navLists.forEach((element) => {
+    element.addEventListener('click', function(){
+        navMenu.style.top = '-50%'
+        menuClose.style.display = 'none'
+        menuOpen.style.display = 'block'
+    })
+})
